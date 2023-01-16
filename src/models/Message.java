a3a1c2971625a0cb113952c6d5e63e995366079d
yasuyuki.kudou <rgst3131@gmail.com>
2023-01-16 18:32:51 +0900

    getAllMessagesを追加

--- src/models/Message.java
+++ src/models/Message.java
@@ -4,13 +4,22 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 
 @Entity
+@NamedQueries({
+    @NamedQuery(
+            name = "getAllMessages",
+            query = "SELECT m FROM Message AS m ORDER BY m.id DESC"
+            )
+})
 @Table(name = "message")
 public class Message {
-    @ID
+    @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;

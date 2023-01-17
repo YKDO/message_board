cdea7c9d502b15baf2a57f8103069b904eaa0f8a
yasuyuki.kudou <rgst3131@gmail.com>
2023-01-17 14:52:38 +0900

    NewServlet.javaを作成

--- src/models/Message.java
+++ src/models/Message.java
@@ -1,5 +1,7 @@
 package models;
 
+import java.sql.Timestamp;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -17,7 +19,7 @@ import javax.persistence.Table;
             query = "SELECT m FROM Message AS m ORDER BY m.id DESC"
             )
 })
-@Table(name = "message")
+@Table(name = "messages")
 public class Message {
     @Id
     @Column(name = "id")
@@ -31,10 +33,10 @@ public class Message {
     private String content;
 
     @Column(name = "created_at", nullable = false)
-    private String created_at;
+    private Timestamp created_at;
 
     @Column(name = "updated_at", nullable = false)
-    private String updated_at;
+    private Timestamp updated_at;
 
     public Integer getId() {
         return id;
@@ -60,19 +62,19 @@ public class Message {
         this.content = content;
     }
 
-    public String getCreated_at() {
+    public Timestamp getCreated_at() {
         return created_at;
     }
 
-    public void setCreated_at(String created_at) {
+    public void setCreated_at(Timestamp created_at) {
         this.created_at = created_at;
     }
 
-    public String getUpdated_at() {
+    public Timestamp getUpdated_at() {
         return updated_at;
     }
 
-    public void setUpdated_at(String updated_at) {
+    public void setUpdated_at(Timestamp updated_at) {
         this.updated_at = updated_at;
     }
 
